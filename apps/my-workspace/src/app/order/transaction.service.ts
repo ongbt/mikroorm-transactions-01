512cf8ac9c1cf84a5eb3e54db0b17fcc0aa01b1b
BT Ong <bengtiong@email.com>
2024-11-05 18:03:53 +0800

    /

--- apps/my-workspace/src/app/order/transaction.service.ts
+++ apps/my-workspace/src/app/order/transaction.service.ts
@@ -1,13 +1,16 @@
 import { EntityManager } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
 import { Order } from '../entities/order.entity';
+import { User } from '../entities/user.entity';
 import { PaymentService } from '../payment/payment.service';
+import { UserService } from '../user/user.service';
 import { OrderService } from './order.service';
 
 @Injectable()
 export class TransactionService {
   constructor(
     private readonly em: EntityManager,
+    private readonly userService: UserService,
     private readonly orderService: OrderService,
     private readonly paymentService: PaymentService
   ) {}
@@ -23,6 +26,20 @@ export class TransactionService {
       const order = await this.orderService.createOrder(productId, amount);
       await this.paymentService.createPayment(amount);
       return order;
+    });
+  }
+  async createUserWithOrderAndPayment(
+    name: string,
+    email: string,
+    productId: string,
+    amount: number
+  ): Promise<User> {
+    return await this.em.transactional(async () => {
+      // Use the same entity manager instance for both services
+      const user = await this.userService.createUser(name, email);
+      await this.createOrderWithPayment(productId, amount);
+
+      return user;
     });
   }
 }
--- apps/my-workspace/src/app/user/user.controller.ts
+++ apps/my-workspace/src/app/user/user.controller.ts
@@ -1,11 +1,32 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { User } from '../entities/user.entity';
+import { TransactionService } from '../order/transaction.service';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly transactionService: TransactionService
+  ) {}
 
+  @Post('transactions')
+  async createTransaction(
+    @Body()
+    body: {
+      name: string;
+      email: string;
+      productId: string;
+      amount: number;
+    }
+  ): Promise<User> {
+    return this.transactionService.createUserWithOrderAndPayment(
+      body.name,
+      body.email,
+      body.productId,
+      body.amount
+    );
+  }
   @Post()
   async create(@Body() body: { name: string; email: string }): Promise<User> {
     return this.userService.createUser(body.name, body.email);
