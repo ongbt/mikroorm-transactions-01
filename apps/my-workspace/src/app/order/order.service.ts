90d2dbb7d2d920494aea0d72040278ec779de243
BT Ong <bengtiong@email.com>
2024-11-07 15:43:00 +0800

    /

--- apps/my-workspace/src/app/order/order.service.ts
+++ apps/my-workspace/src/app/order/order.service.ts
@@ -26,6 +26,11 @@ export class OrderService {
       const order = await this.orderRepository.findOneOrFail(id);
       order.productId = productId;
       order.amount = amount;
+
+      console.log('Start waiting');
+      await this.sleep(1000); // Sleep for 3 seconds
+      console.log('End');
+
       await inner.flush();
       return order;
     });
@@ -36,5 +41,23 @@ export class OrderService {
   async findOne(id: number): Promise<Order> {
     return this.orderRepository.findOneOrFail(id);
   }
+
+  async updateOrder_withWait(
+    id: number,
+    productId: string,
+    amount: number
+  ): Promise<Order> {
+    const order = await this.orderRepository.findOneOrFail(id);
+    order.productId = productId;
+    order.amount = amount;
+    console.log('Start waiting');
+    await this.sleep(1000); // Sleep for 3 seconds
+    console.log('End');
+    await this.em.flush();
+    return order;
+  }
+  sleep = (ms: number): Promise<void> => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
   // Additional methods can be added here for update and delete operations.
 }
